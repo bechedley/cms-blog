3a1630bcd5e02a7fba7f5153ddbb58cc22b573f0
bechedley <109792989+bechedley@users.noreply.github.com>
2022-12-30 19:22:15 +1100

    Add handlebar views

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,3 +1,5 @@
+const { User } = require("../models");
+
 module.exports = {
     format_date: (date) => {
         return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()
@@ -7,4 +9,35 @@ module.exports = {
     format_uppercase: (text) => {
         return `${text.toUpperCase()}`;
     },
+
+    format_author: (data) => {
+
+        if (data == User.id) {
+        return User.username;
+    }},
+
+    format_commentor: (data) => {
+
+        if (data == userData.id) {
+        return User.username;
+    }},
+
+    ifCommentEdit: (data) => {
+
+        if (data == userData.id) {
+            return true;
+        } else {
+            return false;
+        }
+    },
+
+    ifPostEdit: (data) => {
+
+        if (data == userData.id) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 };
